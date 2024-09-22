3ca40be4d7704ee58fd4defcf418b9defe629506
Alexei_1966 <Alexei_1966@outlook.com>
2024-09-22 23:10:41 +0300

    старт

---
+++ TshirtsManager/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TshirtsManager.Data.Models;
+
+namespace TshirtsManager.Data
+{
+    public class AppDbContext : DbContext 
+    {
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+
+        public DbSet<Tshirt> Tshirt { get; set; }
+    }
+}
---
+++ TshirtsManager/Data/Models/Tshirt.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TshirtsManager.Data.Models
+{
+    public class Tshirt
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [MaxLength(150), Column(TypeName = "nvachar(150)")]
+        public string Name { get; set; } = "Name";
+
+        [MaxLength(150), Column(TypeName = "nvachar(150)")]
+        public string Names { get; set; } = "Names";
+
+        [MaxLength(300), Column(TypeName = "nvachar(300)")]
+        public string Description { get; set; } = "Description";
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [MaxLength(100), Column(TypeName = "nvarchar(100)")]
+        public string Adresse { get; set; } = "Adress";
+
+        [MaxLength(10), Column(TypeName = "nvarchar(10)")]
+        public string Time { get; set; } = "12:30";
+
+        public bool Done { get; set; } = false;
+
+        public bool Deleted { get; set; } = false;
+
+        public byte Rating { get; set; } = 2;
+    }
+}
